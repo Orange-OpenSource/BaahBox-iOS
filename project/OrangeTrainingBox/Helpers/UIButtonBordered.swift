7a590aeb2172e05bb287231440b3618d3b11add7
Frédérique Pinson <frederique.pinson@gmail.com>
2025-02-12 10:31:00 +0100

    update copyright year to 2025

--- project/OrangeTrainingBox/Helpers/UIButtonBordered.swift
+++ project/OrangeTrainingBox/Helpers/UIButtonBordered.swift
@@ -2,7 +2,7 @@
 //  UIButtonBordered.swift
 //  Baah Box
 //
-//  Copyright (C) 2017 – 2024 Orange SA
+//  Copyright (C) 2017 – 2025 Orange SA
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
